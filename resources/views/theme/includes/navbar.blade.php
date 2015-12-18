c23504570f5d3a1f5efeac441143a8948b2f9647
Fabio Monti <fabio.monti@gmail.com>
2015-12-18 16:11:49 +0100

    da ufficio

--- resources/views/theme/includes/navbar.blade.php
+++ resources/views/theme/includes/navbar.blade.php
@@ -1,11 +1,14 @@
+
 <div class="navbar">
     <div class="navbar-inner">
-        <a id="logo" href="/">Single Malt</a>
         <ul class="nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/projects">Projects</a></li>
-            <li><a href="/contact">Contact</a></li>
+        @foreach ($voci_menu as $voce_menu)
+
+             <li>{{$voce_menu->voce}}</li>
+        @endforeach
+
+
         </ul>
+
     </div>
 </div>
